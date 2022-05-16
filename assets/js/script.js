2aafb43b1f9b3935c83ab9e75cfef49acf4b5245
Brewalan <sbpk.pro@gmail.com>
2022-05-16 14:18:03 +0200

    Feat burger menu icons fixed

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,9 @@ function toggle() {
     let menuBurger = document.querySelector(".containerHeaderLinks");
     if (menuBurger.style.display === "flex") {
         menuBurger.style.display = "none";
+        document.querySelector("#burgerPic").src="./assets/images/icon-menu.svg";
     } else {
         menuBurger.style.display = "flex";
+        document.querySelector("#burgerPic").src="./assets/images/icon-close-menu.svg";
     }
 }
