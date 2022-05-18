4d7adfe82b69a0915f6372e8cae021a18fdc6192
Brewalan <sbpk.pro@gmail.com>
2022-05-18 18:07:11 +0200

    Feat done with dropdown + some layout

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,22 @@
-function featuresToggle() {
+function toggleFeatures() {
     let features = document.querySelector(".featuresLinks");
     if (features.style.display == "block" ) {
         features.style.display = "none";
+        document.querySelector(".arrowFeatures").innerHTML = "&#9660;";
     } else {
         features.style.display = "block";
+        document.querySelector(".arrowFeatures").innerHTML = "&#9650;";
+    }
+}
+
+function toggleCompany() {
+    let company = document.querySelector(".companyLinks");
+    if (company.style.display == "block" ) {
+        company.style.display = "none";
+        document.querySelector(".arrowCompany").innerHTML = "&#9660;";
+    } else {
+        company.style.display = "block";
+        document.querySelector(".arrowCompany").innerHTML = "&#9650;";
     }
 }
 
